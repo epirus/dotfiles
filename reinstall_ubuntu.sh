5ec55177501f4ab19fcde6af85e6cf52ad05359d
bar <foo>
2017-05-15 17:37:02 +0800

    add ubuntu reinstall script

---
+++ reinstall_ubuntu.sh
@@ -0,0 +1,20 @@
+#Install Basic
+
+sudo apt-get install -y  python3-pip python-pip git tmux openssh-server zsh xclip gitk
+
+#Install  Neovim
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get install neovim
+
+#Install on-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+#Install ibus
+sudo apt-get install ibus ibus-clutter ibus-gtk ibus-gtk3 ibus-qt4&&ibus-setup
+
+
+#Gnome-Terminal Color Configure
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git&&cd gnome-terminal-colors-solarized&&./install.sh
+
+#Configuration
+./link.sh
